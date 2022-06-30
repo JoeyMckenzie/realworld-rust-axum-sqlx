95adbab7ce20382fed33889742d9c79ec7bcd600
Joey McKenzie <joey.mckenzie27@gmail.com>
2022-06-29 19:51:40 -0700

    chore(web): code cleanup

--- bin/conduit-web/src/components/navbar.rs
+++ bin/conduit-web/src/components/navbar.rs
@@ -7,38 +7,12 @@ use crate::router::ConduitRouter;
 
 #[function_component(Navbar)]
 pub fn navbar() -> Html {
-    let location = use_location().expect("location could not be retrieved");
-    let history = use_history().expect("history could not be retrieved");
-
-    {
-        use_effect_with_deps(
-            move |current_location| {
-                let current_path = current_location.pathname();
-                info!("current location: {}", current_path);
-                || ()
-            },
-            location,
-        );
-    }
-
-    {
-        use_effect_with_deps(
-            move |current_history| {
-                let current_path = current_history.location();
-                info!("current location: {}", current_path.pathname());
-                || ()
-            },
-            history,
-        );
-    }
-
     html! {
         <nav class="navbar navbar-light">
             <div class="container">
                 <Link<ConduitRouter> classes="navbar-brand" to={ConduitRouter::Home}>{ "conduit" }</Link<ConduitRouter>>
                 <ul class="nav navbar-nav pull-xs-right">
                     <li class="nav-item">
-                        // <!-- Add "active" class when you're on that page" -->
                         <ActiveLink to={ConduitRouter::Home} display_as="Home" />
                     </li>
                     <li class="nav-item">
